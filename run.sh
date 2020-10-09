5bdfbb4a352ea992c62f69fe839346ce3d11d181
gerald schenk <gschenk@gmx.net>
2020-10-09 22:01:59 +0200

    adds checks if rabbitMQ is running to run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
 
-# check if RabitMQ is up and reached
+# check if RabbitMQ is available
+if ! command -v rabbitmqctl >/dev/null
+then
+  echo "rabitmqctl is not available. RabitMQ seems not to be installed."
+  exit 1
+fi
+
+# test if systemd is available
+if command -v systemctl >/dev/null
+then
+  # check if rabbitmq service is running
+  if ! systemctl is-active --quiet rabbitmq.service
+  then
+    echo "RabitMQ service is not running. 'systemctl start rabitmq.service' may fix it."
+    exit 2
+  fi
+fi
+# some distros (eg Alpine, slackware) do not use systemd, put a different check here
+
+echo "it seems up and running"
 
 # start pv
 
