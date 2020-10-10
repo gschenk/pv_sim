84893d84606025a8c51302b3867a4db71fce8648
gerald schenk <gschenk@gmx.net>
2020-10-10 14:29:57 +0200

    adds pub structs imp to solarize function

--- solarize/src/lib.rs
+++ solarize/src/lib.rs
@@ -1,3 +1,36 @@
+// The insolation struct provides:
+//   flux [W/m^2], instantaneous insolation 
+//   azimuth angle [deg], the angle from north
+//   zenith angle [deg], the angle from zenith
+// 
+pub struct Insolation {
+    pub flux: f64,
+    pub azimuth: f64,
+    pub zenith: f64,
+}
+
+impl Insolation {
+    pub fn new(_day: usize, _time: usize, _lat: usize) -> Insolation {
+        let flux = flux();
+            let azimuth = azimuth();
+            let zenith = zenith();
+        return Insolation{ flux, azimuth, zenith }
+    }
+}
+
+fn flux() -> f64 {
+    return 0.0
+}
+
+fn azimuth() -> f64 {
+    return 0.0
+}
+
+fn zenith() -> f64 {
+    return 0.0
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
