178a15339b5fb3cd2ac20f5577b173a71e4e2150
gerald schenk <gschenk@gmx.net>
2020-10-10 13:33:26 +0200

    adds test, build scripts, adds to run script

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+paths=("pv solarize meter")
+config="config.toml"
+template="config.template.toml"
+debug=true
+
+# build each crate
+for p in ${paths}
+do
+  echo "Build Crate ${p}"
+  cargo build --manifest-path=${p}/Cargo.toml
+done
+
+if [ ! -f $config ]
+then
+  echo "Copying config file. You may like to change it."
+  cp -nv $template $config
+elif [ $debug = true ]
+then
+  echo "Writing over your config file if the template is newer (debug=true)."
+  cp -uv $template $config
+fi
