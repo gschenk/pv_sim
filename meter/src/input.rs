abbc8524b027b83f6722a13c456d15a46b05971b
gerald schenk <gschenk@gmx.net>
2020-10-10 01:35:37 +0000

    Config (#2)
    * adds input.rs to pv, copied from other repo: ...
    
    github.com/gschenk/rustRain
    
    * adds ability to read config file to PV
    
    * refactor errors from input forwarded to main
    
    * corrects wrong name yaml -> toml
    
    * adds test for pv default toml consistency
    
    * structures config file, adds rabbit user, port
    
    * adds config to meter

---
+++ meter/src/input.rs
@@ -0,0 +1,77 @@
+use serde::Deserialize;
+use std::error::Error;
+use std::fs;
+use toml;
+
+const DEFAULT_FILE: &str = "default.toml";
+
+// read file with input data
+fn readfile(file: &str) -> Result<String, Box<dyn Error>> {
+    let contents = fs::read_to_string(file)?;
+    Ok(contents)
+}
+
+// deserialize raw input data
+fn detoml(rawinput: &str) -> Result<Config, Box<dyn Error>> {
+    let parsed: Config = toml::from_str(&rawinput)?;
+    Ok(parsed)
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Config {
+    pub rabbit: Rabbit,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Rabbit {
+    pub user: String,
+    pub address: String,
+    pub port: usize,
+}
+
+// get config from command line arguments
+// looking for filename only
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, Box<dyn Error>> {
+        let filename = if args.len() > 1 {
+            &args[1]
+        } else {
+            DEFAULT_FILE
+        };
+
+        // read contents from config file
+        let contents = readfile(&filename)
+            .map_err(|e| format!("Cannot read configuration file {}. {}", filename, e))?;
+
+        let config = detoml(&contents)
+            .map_err(|e| format!("Cannot parse configuration file {}. {}", filename, e))?;
+
+        Ok(config)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_detoml() {
+        let a: &str = r#"
+                [rabbit]
+                user = "foo"
+                address = '0.0.0.0'
+                port = 5672
+            "#;
+        let expected = detoml(&a).unwrap();
+        println!("{:?}", expected);
+        assert_eq!(expected.rabbit.address, "0.0.0.0".to_string());
+        assert_eq!(expected.rabbit.port, 5672);
+    }
+
+    #[test]
+    fn default_config_consistent() {
+        let config = Config::new(&["bin".to_string()]);
+        println!("{:?}", config);
+        assert!(config.is_ok());
+    }
+}
--- pv/src/main.rs
+++ pv/src/main.rs
@@ -1,6 +1,8 @@
 use serde::Deserialize;
+use std::{env, process};
 
 pub mod consume;
+pub mod input;
 
 #[derive(Deserialize, Debug)]
 pub struct Data {
@@ -9,6 +11,13 @@ pub struct Data {
 }
 
 fn main() {
-    let foo = |x| println!("{:?}", x);
-    let _ = consume::receive(&foo);
+    // read config from file provided as optional CLI argument
+    let args: Vec<String> = env::args().collect();
+    let _config = input::Config::new(&args).unwrap_or_else(|e| {
+        eprintln!("{}", e);
+        process::exit(1)
+    });
+
+    let printer = |x| println!("{:?}", x);
+    let _ = consume::receive(&printer);
 }
