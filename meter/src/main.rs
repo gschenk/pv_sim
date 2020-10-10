22f668e33efbb1cb365a6459320929e67f4e463a
gerald schenk <gschenk@gmx.net>
2020-10-11 01:09:13 +0200

    adds Time struct that provides a mock present time

--- meter/src/main.rs
+++ meter/src/main.rs
@@ -1,9 +1,11 @@
-use amiquip::{Connection, Exchange, Publish, Result};
 use serde::Serialize;
 use std::{env, process};
 
 pub mod input;
 pub mod publisher;
+
+// number of seconds in a day
+const SECONDS_HOUR: u64 = 3600;
 
 #[derive(Serialize)]
 pub struct Data {
@@ -18,10 +20,55 @@ fn main() {
         eprintln!("{}", e);
         process::exit(1)
     });
+
+
+    // looping time
+    let mut time = Time::new(config.time);
+    while let Some(i) = time.now() {
+        println!("{:?}", i);
+    }
+
     let data = Data {
         time: 123456,
         power: std::f64::consts::E,
     };
 
     let _ = publisher::send(data, config.rabbit);
+}
+
+struct Time {
+    time: Option<u64>,
+    stepper: Box<dyn Fn(Option<u64>) -> Option<u64>>,
+}
+
+impl Time {
+    pub fn new(config: input::Time) -> Time {
+        let time = Some(config.start * SECONDS_HOUR);
+
+        // currying stepper with config
+        let stepper = Box::new(timestep(config));
+        return Time{ time, stepper }
+    }
+    pub fn now(&mut self) -> Option<u64> {
+        let time = self.time;
+        let stepper = &self.stepper;
+        self.time = stepper(time);
+        return time;
+    }
+}
+
+fn timestep(config: input::Time) -> impl Fn(Option<u64>) -> Option<u64> {
+    move |t| {
+        let time = match t {
+            Some(t) => t,
+            _ => return None,
+        };
+        let next = time + config.stepsize;
+        let max = config.end * SECONDS_HOUR;
+        return if next <= max {
+            Some(next)
+        } else {
+            None
+        };
+    }
 }
