fb4ac299b7d6e798e0ca93127d5bfa6ba0b65ea0
gerald schenk <gschenk@gmx.net>
2020-10-10 12:11:11 +0000

    Config rabbit (#8)
    * rabbit in PV and Meter service set in config
    
    * adds setting queue to config

--- meter/src/main.rs
+++ meter/src/main.rs
@@ -1,7 +1,7 @@
 use amiquip::{Connection, Exchange, Publish, Result};
 use serde::Serialize;
-use std::{env, process};
 use serde_json;
+use std::{env, process};
 
 pub mod input;
 
@@ -12,16 +12,21 @@ struct Data {
 }
 
 fn main() -> Result<()> {
-
     // read config from filename provided as optional CLI argument
     let args: Vec<String> = env::args().collect();
-    let _config = input::Config::new(&args).unwrap_or_else(|e| {
+    let config = input::Config::new(&args).unwrap_or_else(|e| {
         eprintln!("{}", e);
         process::exit(1)
     });
 
+    // rabbitMQ service parameters are provided by config
+    let ampq_service = &format!(
+        "amqp://{}:{}@{}:{}",
+        config.rabbit.user, config.rabbit.user, config.rabbit.address, config.rabbit.port,
+    );
+
     // Open connection.
-    let mut connection = Connection::insecure_open("amqp://guest:guest@localhost:5672")?;
+    let mut connection = Connection::insecure_open(ampq_service)?;
 
     // Open a channel - None says let the library choose the channel ID.
     let channel = connection.open_channel(None)?;
@@ -36,7 +41,7 @@ fn main() -> Result<()> {
     let ser_foo = serde_json::to_string(&data).unwrap();
 
     // Publish a message to the "hello" queue.
-    exchange.publish(Publish::new(ser_foo.as_bytes(), "foo"))?;
+    exchange.publish(Publish::new(ser_foo.as_bytes(), config.rabbit.queue))?;
 
     connection.close()
 }
