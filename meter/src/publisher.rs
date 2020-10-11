f1caaaf743bef3f88fbcfa86041e1e197aaabd14
gerald schenk <gschenk@gmx.net>
2020-10-11 00:20:27 +0000

    Time (#10)
    * adds config parameters for time
    
    * refactors Meter crate, own module for rabbit
    
    * adds Time struct that provides a mock present time
    
    * refactor moves Time to own module
    
    * Rabbit messages sent now in time propagation loop ...
    
    - input structs get Clone trait
    - time gets a test function
    
    * comments, clean, linting of Meter with time feature

---
+++ meter/src/publisher.rs
@@ -0,0 +1,29 @@
+use amiquip::{Connection, Exchange, Publish, Result};
+use serde_json;
+
+use crate::input::Rabbit;
+use crate::Data;
+
+pub fn send(data: Data, config: &Rabbit) -> Result<()> {
+    // rabbitMQ service parameters are provided by config
+    let ampq_service = &format!(
+        "amqp://{}:{}@{}:{}",
+        config.user, config.user, config.address, config.port,
+    );
+
+    // Open connection.
+    let mut connection = Connection::insecure_open(ampq_service)?;
+
+    // Open a channel - None says let the library choose the channel ID.
+    let channel = connection.open_channel(None)?;
+
+    // Get a handle to the direct exchange on our channel.
+    let exchange = Exchange::direct(&channel);
+
+    let ser_data = serde_json::to_string(&data).unwrap();
+
+    // Publish a message to the "hello" queue.
+    exchange.publish(Publish::new(ser_data.as_bytes(), config.queue.clone()))?;
+
+    connection.close()
+}
