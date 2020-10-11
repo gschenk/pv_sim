bf01380ec81a6c79374cb7b7ea9a7d7d7a5ad28e
gerald schenk <gschenk@gmx.net>
2020-10-11 22:06:24 +0000

    New parms (#12)
    * adds fn to input to identify parameter --quiet
    
    * repalces old Config struct with TomlConfig, ...
    
    ... returns new Config from input::Config::new(.)
    
    * adds --quiet flag to consume in PV
    
    * adds CLI arg changes from PV to Meter
    
    * comments and cleanup

--- meter/src/input.rs
+++ meter/src/input.rs
@@ -5,20 +5,15 @@ use toml;
 
 const DEFAULT_FILE: &str = "default.toml";
 
-// read file with input data
-fn readfile(file: &str) -> Result<String, Box<dyn Error>> {
-    let contents = fs::read_to_string(file)?;
-    Ok(contents)
-}
-
-// deserialize raw input data
-fn detoml(rawinput: &str) -> Result<Config, Box<dyn Error>> {
-    let parsed: Config = toml::from_str(&rawinput)?;
-    Ok(parsed)
-}
-
-#[derive(Deserialize, Debug, Clone)]
+#[derive(Debug, Clone)]
 pub struct Config {
+    pub rabbit: Rabbit,
+    pub time: Time,
+    pub flags: Flags,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct TomlConfig {
     pub rabbit: Rabbit,
     pub time: Time,
 }
@@ -38,25 +33,92 @@ pub struct Time {
     pub end: u64,
 }
 
+#[derive(Debug, Clone)]
+pub struct Flags {
+    pub quiet: bool,
+}
+
 // get config from command line arguments
 // looking for filename only
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, Box<dyn Error>> {
-        let filename = if args.len() > 1 {
-            &args[1]
-        } else {
-            DEFAULT_FILE
-        };
+        let flags = flags_from_args(&args);
+        let filename = file_from_args(&args);
 
         // read contents from config file
         let contents = readfile(&filename)
             .map_err(|e| format!("Cannot read configuration file {}. {}", filename, e))?;
 
-        let config = detoml(&contents)
+        let TomlConfig { rabbit, time } = detoml(&contents)
             .map_err(|e| format!("Cannot parse configuration file {}. {}", filename, e))?;
 
-        Ok(config)
+        Ok(Config {
+            rabbit,
+            time,
+            flags,
+        })
     }
+}
+
+// Processing CLI Arguments
+
+// constructs default Flags struct and has a method for
+// each field that can be toggled
+impl Flags {
+    // new Flags struct with all fields set to default
+    fn new() -> Flags {
+        return Flags { quiet: false };
+    }
+    fn quiet(&mut self) {
+        self.quiet = true;
+    }
+}
+
+// check if a string starts with a char that symbols a comand line flag
+fn is_flag(s: &str) -> bool {
+    return match s.chars().next() {
+        Some('-') => true,
+        _ => false,
+    };
+}
+
+// returns first argument that is not a cli flag
+// or default file if empty
+fn file_from_args(args: &[String]) -> &str {
+    let foo = args.iter().skip(1).find(|s| !is_flag(s));
+
+    return match foo {
+        Some(s) => s,
+        _ => DEFAULT_FILE,
+    };
+}
+
+// returns Flags struct with flags set for CLI flags it finds in its
+// argument list
+fn flags_from_args(args: &[String]) -> Flags {
+    let mut flags = Flags::new();
+    let _ = args.iter().skip(1).filter(|s| is_flag(s)).for_each(|s| {
+        match &**s {
+            "-q" => flags.quiet(),
+            "--quiet" => flags.quiet(),
+            _ => {}
+        };
+    });
+    return flags;
+}
+
+// Reading and Deserializing
+
+// read file with input data
+fn readfile(file: &str) -> Result<String, Box<dyn Error>> {
+    let contents = fs::read_to_string(file)?;
+    Ok(contents)
+}
+
+// deserialize raw input data
+fn detoml(rawinput: &str) -> Result<TomlConfig, Box<dyn Error>> {
+    let parsed: TomlConfig = toml::from_str(&rawinput)?;
+    Ok(parsed)
 }
 
 #[cfg(test)]
