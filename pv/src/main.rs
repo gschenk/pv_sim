eff26a6cbce650e859081d866ef8039c80372db5
gerald schenk <gschenk@gmx.net>
2020-10-11 21:50:43 +0000

    Sums (#11)
    * fixes: season on north and south hemisphere are exchanged
    
    * pv now returns power as function of time
    
    * adds inclination and alignment to panel
    
    * adds calculation for panel size, PV returns panel power now
    
    Signed-off-by: gerald schenk <gschenk@gmx.net>
    
    * fix: attenuation by air mass was wrong
    
    Signed-off-by: gerald schenk <gschenk@gmx.net>
    
    * refactor: moves PV power functions to own module

--- pv/src/main.rs
+++ pv/src/main.rs
@@ -1,14 +1,22 @@
 use serde::Deserialize;
 use std::{env, process};
-use solarize::Insolation;
 
 pub mod consume;
 pub mod input;
+mod power;
 
 #[derive(Deserialize, Debug)]
 pub struct Data {
-    time: usize,
+    time: u64,
     power: f64,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Panel {
+    alignment: f64,   // 180 facing south, 0 facing north [deg]
+    inclination: f64, //angle from horizontal [deg]
+    peak: f64,        // nominal power of installed PV panels [kW]
+    efficiency: f64,  // ratio output to photonic input
 }
 
 fn main() {
@@ -19,8 +27,37 @@ fn main() {
         process::exit(1)
     });
 
-    let insolation = Insolation::new(0,0,0.0);
+    let panel = Panel {
+        alignment: 90.0,
+        inclination: 30.0,
+        peak: 10.0,
+        efficiency: 0.15,
+    };
 
-    let printer = |x| println!("{:?} {}", x, insolation.azimuth );
-    let _ = consume::receive(&printer, config.rabbit);
+    // this closure is going to be passed to rabbit consumer
+    // to process each incomming message immediately
+    let process = |meter: Data| {
+        // Mock time from the meter is passed to the solar function.  This is
+        // problmatic in some ways:
+        // - real world this would just get the present value as the PV
+        //   electronics measures it.
+        // - when properly mocking this (and doing so at speed) we ought to use
+        //   a common time source (eg mocking an NTSC) since we use asynchronous
+        //   messaging
+        // - for actual applications the asynchronicity between PV and metering
+        //   may be problematic in edge cases where PV from the panels changes
+        //   on a fast time-scale eg due to shading or when controlling cos(phi)
+        //   as well.
+
+        // calculate solar power of PV
+        let solar = power::solar(150, meter.time, 45.0, &panel);
+
+        // format output
+        let output = format!("{} {}", meter.time, solar + meter.power);
+
+        // write to STDOUT
+        println!("{}", output);
+    };
+    //let printer = |x| println!("{:?} {}", x, insolation.azimuth );
+    let _ = consume::receive(&process, config.rabbit);
 }
