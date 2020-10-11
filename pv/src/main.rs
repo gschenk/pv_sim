65c9793cb35b87d9fab3f051e62733c884c204e1
gerald schenk <gschenk@gmx.net>
2020-10-12 01:48:51 +0200

    refactor: moves PV timestamp to own module

--- pv/src/main.rs
+++ pv/src/main.rs
@@ -1,11 +1,10 @@
-use chrono::{Duration, NaiveDate, NaiveDateTime};
 use serde::Deserialize;
-use std::convert::TryInto;
 use std::{env, process};
 
 pub mod consume;
 pub mod input;
 mod power;
+mod timestamp;
 
 #[derive(Deserialize, Debug)]
 pub struct Data {
@@ -42,7 +41,7 @@ fn main() {
         let solar = power::solar(meter.day, meter.time, 45.0, &config.panel);
 
         // timestamps
-        let timestamp = seconds_to_timestamp(meter.time, meter.day, meter.year);
+        let timestamp = timestamp::from_s_d_y(meter.time, meter.day, meter.year);
 
         // format output
         let output = format!("{} {}", timestamp, solar + meter.power);
@@ -52,19 +51,4 @@ fn main() {
     };
     //let printer = |x| println!("{:?} {}", x, insolation.azimuth );
     let _ = consume::receive(&process, &config);
-}
-
-// calculates iso timestamp for time s, day, year
-fn seconds_to_timestamp(seconds: u64, day: u64, year: u64) -> NaiveDateTime{
-    let iyear: i32 = year.try_into().unwrap();
-    let iday: i64 = day.try_into().unwrap();
-    let iseconds: i64 = seconds.try_into().unwrap();
-
-    let date: NaiveDateTime = NaiveDate::from_ymd(iyear, 1, 1)
-        .and_hms(0, 0, 0)
-        .checked_add_signed(Duration::days(iday))
-        .unwrap()
-        .checked_add_signed(Duration::seconds(iseconds))
-        .unwrap();
-    return date;
 }
