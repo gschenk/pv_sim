2c0cb9118b684d285b5e380f8808262fac97959c
gerald schenk <gschenk@gmx.net>
2020-10-11 00:20:27 +0000

    Time (#10)
    * adds config parameters for time
    
    * refactors Meter crate, own module for rabbit
    
    * adds Time struct that provides a mock present time
    
    * refactor moves Time to own module
    
    * Rabbit messages sent now in time propagation loop ...
    
    - input structs get Clone trait
    - time gets a test function
    
    * comments, clean, linting of Meter with time feature

--- meter/src/main.rs
+++ meter/src/main.rs
@@ -1,17 +1,17 @@
-use amiquip::{Connection, Exchange, Publish, Result};
 use serde::Serialize;
-use serde_json;
 use std::{env, process};
 
 pub mod input;
+mod publisher;
+mod time;
 
 #[derive(Serialize)]
-struct Data {
-    time: usize,
+pub struct Data {
+    time: u64,
     power: f64,
 }
 
-fn main() -> Result<()> {
+fn main() {
     // read config from filename provided as optional CLI argument
     let args: Vec<String> = env::args().collect();
     let config = input::Config::new(&args).unwrap_or_else(|e| {
@@ -19,29 +19,19 @@ fn main() -> Result<()> {
         process::exit(1)
     });
 
-    // rabbitMQ service parameters are provided by config
-    let ampq_service = &format!(
-        "amqp://{}:{}@{}:{}",
-        config.rabbit.user, config.rabbit.user, config.rabbit.address, config.rabbit.port,
-    );
+    // closure for configuration on rabbitMQ publisher
+    let publish = |data| publisher::send(data, &config.rabbit);
 
-    // Open connection.
-    let mut connection = Connection::insecure_open(ampq_service)?;
+    // `time` mocks a proper time source
+    let mut time = time::Time::new(&config.time);
 
-    // Open a channel - None says let the library choose the channel ID.
-    let channel = connection.open_channel(None)?;
-
-    // Get a handle to the direct exchange on our channel.
-    let exchange = Exchange::direct(&channel);
-
-    let data = Data {
-        time: 123456,
-        power: std::f64::consts::E,
-    };
-    let ser_foo = serde_json::to_string(&data).unwrap();
-
-    // Publish a message to the "hello" queue.
-    exchange.publish(Publish::new(ser_foo.as_bytes(), config.rabbit.queue))?;
-
-    connection.close()
+    // looping time
+    while let Some(now) = time.now() {
+        // this struct is going to be sent back
+        let data = Data {
+            time: now,  // time of day in seconds
+            power: 0.0, // power consumption in W
+        };
+        let _ = &publish(data);
+    }
 }
