3899c7e18fd899a5828239d16f9b52603fd61da2
gerald schenk <gschenk@gmx.net>
2020-10-11 20:31:36 +0200

    adds fn to input to identify parameter --quiet

--- pv/src/input.rs
+++ pv/src/input.rs
@@ -34,11 +34,9 @@ pub struct Rabbit {
 // looking for filename only
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, Box<dyn Error>> {
-        let filename = if args.len() > 1 {
-            &args[1]
-        } else {
-            DEFAULT_FILE
-        };
+
+        let _flags = flags_from_args(&args);
+        let filename = file_from_args(&args);
 
         // read contents from config file
         let contents = readfile(&filename)
@@ -49,6 +47,55 @@ impl Config {
 
         Ok(config)
     }
+}
+
+// check if a string starts with a char that symbols a comand line flag
+fn is_flag(s: &str) -> bool {
+    return match s.chars().next() {
+        Some('-') => true,
+        _ => false,
+    };
+}
+
+// returns first argument that is not a cli flag
+// or default file if empty
+fn file_from_args(args: &[String]) -> &str {
+    let foo = args.iter().skip(1).find(|s| !is_flag(s));
+
+    return match foo {
+        Some(s) => s,
+        _ => DEFAULT_FILE,
+    };
+}
+
+pub struct Flags {
+    quiet: bool,
+}
+
+impl Flags {
+    // new Flags struct with all fields set to default
+    fn new() -> Flags {
+        return Flags { quiet: false };
+    }
+    fn quiet(&mut self) {
+        self.quiet = true;
+    }
+}
+
+fn flags_from_args(args: &[String]) -> Flags {
+    let mut flags = Flags::new();
+    let _ = args
+        .iter()
+        .skip(1)
+        .filter(|s| is_flag(s))
+        .for_each(|s| {
+            match &**s {
+                "-q" => flags.quiet(),
+                "--quiet" => flags.quiet(),
+                _ => {}
+            };
+        });
+    return flags;
 }
 
 #[cfg(test)]
