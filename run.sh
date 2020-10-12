27e2b702ed6ef6b115ab55cb6f8cd94a80732d6b
gerald schenk <gschenk@gmx.net>
2020-10-12 07:52:39 +0200

    adds check for localhost in config to run script

--- run.sh
+++ run.sh
@@ -1,26 +1,31 @@
 #!/bin/sh
 
-#
+config="config.toml"
 default_output="results.dat"
 
-# check if RabbitMQ is available
-if ! command -v rabbitmqctl >/dev/null
+# check if localhost is configured (not checking for 127..)
+if grep -q localhost $config
 then
-  echo "rabitmqctl is not available. RabitMQ seems not to be installed."
-  exit 1
-fi
 
-# test if systemd is available
-if command -v systemctl >/dev/null
-then
-  # check if rabbitmq service is running
-  if ! systemctl is-active --quiet rabbitmq.service
+  # check if RabbitMQ is available
+  if ! command -v rabbitmqctl >/dev/null
   then
-    echo "RabitMQ service is not running. 'systemctl start rabitmq.service' may fix it."
-    exit 2
+    echo "rabitmqctl is not available. RabitMQ seems not to be installed."
+    exit 1
   fi
+
+  # test if systemd is available
+  if command -v systemctl >/dev/null
+  then
+    # check if rabbitmq service is running
+    if ! systemctl is-active --quiet rabbitmq.service
+    then
+      echo "RabitMQ service is not running. 'systemctl start rabitmq.service' may fix it."
+      exit 2
+    fi
+  fi
+  # some distros (eg Alpine, slackware) do not use systemd, put a different check here
 fi
-# some distros (eg Alpine, slackware) do not use systemd, put a different check here
 
 output=${1:-"$default_output"}
 
